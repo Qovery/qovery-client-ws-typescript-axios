590f456efb0b5ffaa8bc8433d161edbeb91e0d3f
qovery <github@qovery.com>
2025-05-27 11:51:27 +0000

    See https://github.com/Qovery/qovery-openapi-spec/commit/feb0dc3d1e7d4a6da9e0242f8869bdd45a7b02d1 from refs/heads/main

--- api.ts
+++ api.ts
@@ -79,7 +79,7 @@ export interface CertificateStatusDto {
      * @type {number}
      * @memberof CertificateStatusDto
      */
-    'last_failure_issuance_time'?: number | null;
+    'last_failure_issuance_time': number;
     /**
      * 
      * @type {number}
@@ -970,13 +970,13 @@ export interface NodeResourceAllocatedDto {
      * @type {number}
      * @memberof NodeResourceAllocatedDto
      */
-    'limit_cpu_milli'?: number | null;
+    'limit_cpu_milli': number;
     /**
      * 
      * @type {number}
      * @memberof NodeResourceAllocatedDto
      */
-    'limit_memory_mib'?: number | null;
+    'limit_memory_mib': number;
     /**
      * 
      * @type {number}
@@ -1520,22 +1520,10 @@ export interface QoveryNodeFailure {
     'message': string;
     /**
      * 
-     * @type {QoveryNodeAttributes}
-     * @memberof QoveryNodeFailure
-     */
-    'node_attributes': QoveryNodeAttributes;
-    /**
-     * 
      * @type {string}
      * @memberof QoveryNodeFailure
      */
     'reason': string;
-    /**
-     * 
-     * @type {string}
-     * @memberof QoveryNodeFailure
-     */
-    'type': string;
 }
 /**
  * 
@@ -1842,6 +1830,117 @@ export class ClusterListNodesApi extends BaseAPI {
      */
     public handleClusterListNodesRequest(organization: string, cluster: string, options?: RawAxiosRequestConfig) {
         return ClusterListNodesApiFp(this.configuration).handleClusterListNodesRequest(organization, cluster, options).then((request) => request(this.axios, this.basePath));
+    }
+}
+
+
+
+/**
+ * ClusterMetricsApi - axios parameter creator
+ * @export
+ */
+export const ClusterMetricsApiAxiosParamCreator = function (configuration?: Configuration) {
+    return {
+        /**
+         * 
+         * @param {string} organization 
+         * @param {string} cluster 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        handleClusterMetricsRequest: async (organization: string, cluster: string, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'organization' is not null or undefined
+            assertParamExists('handleClusterMetricsRequest', 'organization', organization)
+            // verify required parameter 'cluster' is not null or undefined
+            assertParamExists('handleClusterMetricsRequest', 'cluster', cluster)
+            const localVarPath = `/cluster/metrics`
+                .replace(`{${"organization"}}`, encodeURIComponent(String(organization)))
+                .replace(`{${"cluster"}}`, encodeURIComponent(String(cluster)));
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'GET', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+
+    
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+    }
+};
+
+/**
+ * ClusterMetricsApi - functional programming interface
+ * @export
+ */
+export const ClusterMetricsApiFp = function(configuration?: Configuration) {
+    const localVarAxiosParamCreator = ClusterMetricsApiAxiosParamCreator(configuration)
+    return {
+        /**
+         * 
+         * @param {string} organization 
+         * @param {string} cluster 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async handleClusterMetricsRequest(organization: string, cluster: string, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<ClusterStatusDto>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.handleClusterMetricsRequest(organization, cluster, options);
+            const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
+            const localVarOperationServerBasePath = operationServerMap['ClusterMetricsApi.handleClusterMetricsRequest']?.[localVarOperationServerIndex]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
+        },
+    }
+};
+
+/**
+ * ClusterMetricsApi - factory interface
+ * @export
+ */
+export const ClusterMetricsApiFactory = function (configuration?: Configuration, basePath?: string, axios?: AxiosInstance) {
+    const localVarFp = ClusterMetricsApiFp(configuration)
+    return {
+        /**
+         * 
+         * @param {string} organization 
+         * @param {string} cluster 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        handleClusterMetricsRequest(organization: string, cluster: string, options?: RawAxiosRequestConfig): AxiosPromise<ClusterStatusDto> {
+            return localVarFp.handleClusterMetricsRequest(organization, cluster, options).then((request) => request(axios, basePath));
+        },
+    };
+};
+
+/**
+ * ClusterMetricsApi - object-oriented interface
+ * @export
+ * @class ClusterMetricsApi
+ * @extends {BaseAPI}
+ */
+export class ClusterMetricsApi extends BaseAPI {
+    /**
+     * 
+     * @param {string} organization 
+     * @param {string} cluster 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof ClusterMetricsApi
+     */
+    public handleClusterMetricsRequest(organization: string, cluster: string, options?: RawAxiosRequestConfig) {
+        return ClusterMetricsApiFp(this.configuration).handleClusterMetricsRequest(organization, cluster, options).then((request) => request(this.axios, this.basePath));
     }
 }
 
